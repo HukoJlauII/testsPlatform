7e15db2b1cf837e6bc1ec82c73402c5fcf39a0dd
Khakimov Renat <hakimov_2001@mail.ru>
2022-12-13 11:27:55 +0300

    timetable adding

---
+++ src/main/java/com/example/testsplatform/controller/TimeTableController.java
@@ -0,0 +1,22 @@
+package com.example.testsplatform.controller;
+
+import com.example.testsplatform.util.MireaParser.ParserXLSX;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+import net.minidev.json.parser.ParseException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class TimeTableController {
+
+
+    @PostMapping("/getTimeTable")
+    @ResponseBody
+    public Object getTimeTable(@RequestBody String jsonString) throws ParseException {
+        JSONObject json = (JSONObject) new JSONParser().parse(jsonString);
+        return ParserXLSX.groups.get((String) json.get("searchLine")).
+                get((Integer)json.get("week")).
+                get((Integer) json.get("day"));
+    }
+}
--- src/main/java/com/example/testsplatform/util/MireaParser/ParserXLSX.java
+++ src/main/java/com/example/testsplatform/util/MireaParser/ParserXLSX.java
@@ -3,30 +3,34 @@ package com.example.testsplatform.util.MireaParser;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.minidev.json.JSONObject;
+import netscape.javascript.JSObject;
 import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.ss.usermodel.DateUtil;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.event.ApplicationContextInitializedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 @Component
 @Order(3)
 public class ParserXLSX implements CommandLineRunner {
 
     public static Map<String, Map<Integer, ArrayList<ArrayList<Map<String, String>>>>> groups = new HashMap<>();
+
+    public static JSONObject object;
 
     public void huy() throws IOException {
         try {
@@ -38,47 +42,46 @@ public class ParserXLSX implements CommandLineRunner {
                 XSSFWorkbook book = new XSSFWorkbook(pkg);
                 XSSFSheet sheet = book.getSheetAt(0);
                 readCells(sheet);
-                book.close();
+                pkg.close();
             }
 
-            JSONObject object = new JSONObject(groups);
-            try (Writer writer = new FileWriter("Output.json")) {
-                Gson gson = new GsonBuilder().create();
-                gson.toJson(object, writer);
-            }
+            object = new JSONObject(groups);
+//            try (Writer writer = new FileWriter("Output.json")) {
+//                Gson gson = new GsonBuilder().create();
+//                gson.toJson(object, writer);
+//            }
 
         } catch (EncryptedDocumentException | IOException e) {
-            e.printStackTrace();
+             e.printStackTrace();
         } catch (InvalidFormatException e) {
             throw new RuntimeException(e);
         }
 
     }
 
-    static ArrayList<ArrayList<Map<String, String>>> constructWeek() {
+    static ArrayList<ArrayList<Map<String, String>>> constructWeek(){
         ArrayList<ArrayList<Map<String, String>>> elem = new ArrayList<>(6);
         for (int i = 0; i < 6; i++)
             elem.add(new ArrayList<>(7));
         return elem;
     }
-
     private static void readCells(XSSFSheet sheet) throws IOException {
 
 
         short maxCol = sheet.getRow(0).getLastCellNum();
 
-        for (int col = 5; col < maxCol; col = col + 5) {
+        for(int col = 5; col < maxCol ; col = col + 5) {
 
             Map<Integer, ArrayList<ArrayList<Map<String, String>>>> week = new HashMap<>();
+            week.put(0, constructWeek());
             week.put(1, constructWeek());
-            week.put(2, constructWeek());
 
             // Встретился день недели
             if (col % 15 == 0)
                 continue;
             String groupName = printCell(sheet.getRow(1).getCell(col));
 
-            for (int rw = 3; rw < 85; rw++) {
+            for (int rw = 3; rw < 85; rw ++) {
                 XSSFCell cell = sheet.getRow(rw).getCell(col);
                 XSSFCell cell2 = sheet.getRow(rw).getCell(col + 1);
                 XSSFCell cell3 = sheet.getRow(rw).getCell(col + 2);
@@ -89,13 +92,12 @@ public class ParserXLSX implements CommandLineRunner {
                 subject.put("type", printCell(cell2).replaceAll("\n", ""));
                 subject.put("teacher", printCell(cell3).replaceAll("\n", ""));
                 subject.put("audit", printCell(cell4).replaceAll("\n", ""));
-                week.get(rw % 2 + 1).get(((rw - 3) / 14) % 6).add(subject);
+                week.get(rw % 2).get(((rw - 3) / 14) % 6).add(subject);
             }
             groups.put(groupName, week);
         }
     }
-
-    private static String printCell(XSSFCell cell) {
+    private static String printCell(XSSFCell cell){
 
         // Вывод значения в консоль
         switch (cell.getCellType()) {
