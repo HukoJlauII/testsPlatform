0f98f4cb0eb489ab37b92800b5a8501c7b4dffec
Nikita <nzhigulevskiy@bk.ru>
2022-12-14 08:21:10 +0300

    small changes №1

--- src/main/resources/static/assets/js/timeTable.js
+++ src/main/resources/static/assets/js/timeTable.js
@@ -35,24 +35,21 @@ function createAjaxQuery(url, data, toFunction) {
 
 function successFindGroup(data) {
     console.log(data)
-    const arr = ['9:00 - 10:30', '10:40 - 12:10', '12:40 - 14:10', '14:20 - 15:50', '16:20 - 17:50', '18:00 - 19:30', '19:40 - 21:00']
-    console.log(div.children.length)
-    if (div.children.length === 7 && div.children.length !== 0) {
-        while (div.firstChild) {
-            div.removeChild(div.firstChild);
-        }
-    }
-    for (let i = 0; i < data.length; i++) {
+    const arr = ['9:00 - 10:30', '10:40 - 12:10','12:40 - 14:10','14:20 - 15:50','16:20 - 17:50','18:00 - 19:30','19:40 - 21:00']
+    console.log(div.children.length )
+    div.innerHTML='';
+    for (let i = 0; i <= data.length; i++) {
         const table = document.createElement("tr");
         let code = "";
         i++;
         code += "<tr>\n";
         code += "<th scope=\"row\"><a href=\"#\">" + i + "</a></th>\n";
         i--;
-        code += "<td>" + arr[i] + "</td>\n";
-        code += "<td><a href=\"#\" class=\"text-primary\">" + data[i].title + "</a></td>\n";
-        code += "<td>" + data[i].audit + "</td>\n";
-        code += "<td><span class=\"badge bg-success\">" + data[i].teacher + "</span></td>\n";
+        code += "<td>"+arr[i]+"</td>\n";
+        code += "<td><a href=\"#\" class=\"text-primary\">"+data[i].title+"</a></td>\n";
+        code += "<td>"+data[i].type+"</td>\n";
+        code += "<td>"+data[i].audit+"</td>\n";
+        code += "<td><span class=\"badge bg-success\">"+data[i].teacher+"</span></td>\n";
         code += "</tr>\n";
         console.log(code)
         table.innerHTML = code;
